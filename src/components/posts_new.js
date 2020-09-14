6ae636a49894a4beec79595e2ec946237124e2a6
Matthew Denune <mattdenune@gmail.com>
2020-09-14 17:58:03 -0400

    working on getting input value to show on screen

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-    renderTitleField(field) {
+    renderField(field) {
        return(
-            <div>
+            <div className='form-group'>
+                <label htmlFor="">{field.label}</label>
                 <input 
+                    className='form-control'
                     type='text'
                     {...field.input}
                 />
@@ -13,12 +15,25 @@ class PostsNew extends Component {
        ); 
     }
 
+   
+
     render() {
         return (
             <form>
                 <Field 
+                    label='Title for Post'
                     name='title'
-                    component={this.renderTitleField}
+                    component={this.renderField}
+                />
+                <Field 
+                    label='Categories'
+                    name='categories'
+                    component={this.renderField}
+                />
+                <Field 
+                    label='Post Content'
+                    name='content'
+                    component={this.renderField}
                 />
             </form>
         )
