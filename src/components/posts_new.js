2ceb33274c33e86d4d56c63e2fd22853dce457c5
Matthew Denune <mattdenune@gmail.com>
2020-09-22 17:44:52 -0400

    created new post and delete functionality

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
+        const { meta: { touched, error }} = field;
+        const className = `form-control ${ touched && error ? 'border border-danger' : "" }`;
        return(
             <div className='form-group'>
                 <label htmlFor="">{field.label}</label>
                 <input 
-                    className='form-control'
+                    className={className}
                     type='text'
                     {...field.input}
                 />
+                <div className="text-danger">
+                    {touched ? error : ""}
+                </div>
             </div> 
        ); 
     }
 
-   
+   onSubmit(values) {
+       this.props.createPost(values, () => {
+           this.props.history.push('/');
+       });
+   }
 
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field 
                     label='Title for Post'
                     name='title'
@@ -35,11 +49,37 @@ class PostsNew extends Component {
                     name='content'
                     component={this.renderField}
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to='/' className="btn btn-danger ml-2">Cancel</Link>
             </form>
         )
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    // Validate the inputs from 'values'
+    if (!values.title) {
+        errors.title = 'Enter a title';
+    }
+    if (!values.categories) {
+        errors.categories = 'Enter some categories';
+    }
+    if (!values.content) {
+        errors.content = 'Enter some content';
+    }
+
+
+    // if errors is empty, the form is fine to submit.
+    // If errors has *any* propertiesm, redux form assumees form is invalid.
+    return errors;
+}
+
+
 export default reduxForm({
+    validate: validate,
     form: 'PostsNewForm'
-})(PostsNew)
+})(
+    connect(null, {createPost})(PostsNew)
+);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,17 @@
-import { FETCH_POSTS } from '../actions';
 import _ from 'lodash';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function(state={}, action) {
     switch (action.type) {
+        case FETCH_POST: 
+        // OLD WAY
+        // const post = action.payload.data;
+        // const newState =  {...state };
+        // newState[post.id] = post;
+        // return newState;
+
+        // NEW WAY
+        return {...state, [action.payload.data.id]: action.payload.data};
         case FETCH_POSTS:
             // converts our array of objects into an object of objects
             // that uses the id as a key for each entry
